c4ef6e6feb0428f448fc5cf64d47b1a8e862c19a
Robert Sarkozi <robert.sarkoezi@softing.com>
2022-04-14 09:49:38 +0300

    Removed target, added .gitignore

--- enum_promote_mem_take/target/debug/deps/enum_promote_mem_take-5e36390b030b9732.d
+++
@@ -1,5 +0,0 @@
-/home/robcsi/git/rust/enum_promote_mem_take/target/debug/deps/enum_promote_mem_take-5e36390b030b9732: src/main.rs
-
-/home/robcsi/git/rust/enum_promote_mem_take/target/debug/deps/enum_promote_mem_take-5e36390b030b9732.d: src/main.rs
-
-src/main.rs:
--- enum_promote_mem_take/target/debug/enum_promote_mem_take.d
+++
@@ -1,1 +0,0 @@
-/home/robcsi/git/rust/enum_promote_mem_take/target/debug/enum_promote_mem_take: /home/robcsi/git/rust/enum_promote_mem_take/src/main.rs
